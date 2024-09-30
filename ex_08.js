d9e5df0b9d0602d40d247529573b4c83dacdf30f
sarah1reindl <sarah1.reindl@epitech.digital>
2024-09-30 15:25:17 +0200

    ex_08.js

---
+++ ex_08.js
@@ -0,0 +1,13 @@
+if (breadCount >= 2 && hamCount >= 1 && tunaCount >=1) {
+displayThisText("I can make a royal sandwitch!");
+}
+else if (breadCount >= 2 && hamCount >= 1) {
+displayThisText("I can make a ham sandwitch!");
+}
+else if(breadCount >= 2 && tunaCount >= 1) {
+displayThisText("I can make a tuna sandwitch!");
+}
+
+else {  ("I'd rather be fasting tonight")
+
+}
