9a002b91e66a2ba99dbedf40e26c18bec861a4e6
sarah1reindl <sarah1.reindl@epitech.digital>
2024-09-30 10:52:37 +0200

    ex_01.js

---
+++ ex_01.js
@@ -0,0 +1,9 @@
+function displayThisText() {
+ var greetings = text
+    var text = "Hello World!";
+    document.getElementById("out").innerHTML = text;
+    stepAnimateText('.fade', 'bounceInDown', 0.02);
+}
+ {
+    displayThisText();
+};
