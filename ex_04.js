cf75226980b690a7d1ba2cef49c12ca83395f7bb
sarah1reindl <sarah1.reindl@epitech.digital>
2024-09-30 12:34:40 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,8 @@
+displayThisText(typeof myhouse);
+displayThisText(typeof false);
+displayThisText(typeof 3);
+displayThisText(typeof 140n);
+displayThisText(typeof Symbol());
+displayThisText(typeof {})
+displayThisText(typeof $)
+
