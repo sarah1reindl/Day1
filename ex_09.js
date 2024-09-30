fd08aac4e7c37c3b42703c58426a44da849481fd
sarah1reindl <sarah1.reindl@epitech.digital>
2024-09-30 16:44:51 +0200

    ex_09.js

--- ex_09.js
+++ ex_09.js
@@ -1,17 +1,7 @@
-let type;
-if (typeof input_var === "string") {
-    type = "String";
-} else if (typeof input_var === "boolean") {
-    type = "Boolean";
-} else if (Number.isInteger(input_var)) {
-    type = "Integer";
-} else if (typeof input_var === "number") {
-    type = "Float";
-}
+displayThisText("The variable value is " + "'" + input_var + "'" + ".");
+displayThisText("Its type is " + "'" + typeof(input_var) + "'" + ".");
 
-displayThisText(`The variable value is '${input_var}'`);
-displayThisText(`Its type is '${type}'`);
-
-if (input_var === 42 || input_var === "42") {
-    displayThisText("It is the meaning of life");
+if (input_var === 42)
+{
+    displayThisText("It is the meaning of life.");
 }
