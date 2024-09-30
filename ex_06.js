708ee48089c27734e5b7d9bd955b4d46ef571323
sarah1reindl <sarah1.reindl@epitech.digital>
2024-09-30 14:33:22 +0200

    ex_06.js

---
+++ ex_06.js
@@ -0,0 +1,13 @@
+if (bananasCount  === 0) {
+ 
+ console.log( "Oh no, there is no banana" );
+ 
+ } else if (bananasCount > 0) {
+ 
+ console.log("Yummy!");
+ 
+ 
+ }else {
+ 
+ console.log("Do i owe you bananas?")
+ }
