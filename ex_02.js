9ae074a00d867bb3746526a4b127088ca09e1f1a
sarah1reindl <sarah1.reindl@epitech.digital>
2024-09-30 11:26:23 +0200

    Create ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,12 @@
+let integer = ("true<br>");
+ document.write(integer)
+let float = ("forty two<br>");
+document.write(float)
+let string = ("42<br>");
+document.write(string)
+let bool = null;
+document.write(bool)
+let this_is_null = "42.42<br>";
+document.write(this_is_null)
+
+
